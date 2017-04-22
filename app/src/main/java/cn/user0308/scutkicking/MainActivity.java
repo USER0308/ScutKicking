0d8aed0b945e96e0fb0f762f58a392b2bce4fc83
satoko1997 <1963023046@qq.com>
2017-04-22 20:19:27 +0800

    satoko的改动

--- app/src/main/java/cn/user0308/scutkicking/MainActivity.java
+++ app/src/main/java/cn/user0308/scutkicking/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     //ball ball
     //ball ball
+    //jksjdkfj
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
